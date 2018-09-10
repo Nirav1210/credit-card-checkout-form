c29a9f4d5a52f3644ca581e876d0f68617056c9c
Nirav1210 <nirav.bhut1210@gmail.com>
2018-09-10 01:48:11 -0400

    add credit card validation

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="form-container">
+    <!--<div v-for="error in errors" :key="error">
+      <b-alert variant="danger" :show="isError">{{ error }}</b-alert>
+    </div>-->
     <b-form @submit="onSubmit" name="creditCardForm">
       <div class="left-container">
-        <b-form-group label="CARD NUMBER">
+        <b-form-group label="CARD NUMBER" :invalid-feedback="errors.cardNumber" :state="errors.cardNumber.length > 0 ? false : null">
           <b-row>
             <b-col v-for="(item, index) in numberSets" :key="index">
-              <b-form-input type="text" maxlength="4" placeholder="0000"
+              <b-form-input class="inputs" type="text" maxlength="4"
+                            :id="`${index}`"
                             v-model="numberSets[index]"
                             v-numericOnly
-                            required>
+                            v-autoTab
+                            required
+                            placeholder="0000"
+                            :state="errors.cardNumber.length > 0 ? false : null">
               </b-form-input>
             </b-col>
           </b-row>
@@ -35,21 +42,27 @@
         </b-row>
         <b-row>
           <b-col>
-            <b-form-group label="MONTH">
+            <b-form-group label="MONTH" :invalid-feedback="errors.expiryMonth" :state="errors.expiryMonth.length > 0 ? false : null">
               <b-form-input type="text"
+                            id="expiryMonth"
                             v-numericOnly maxlength="2"
                             v-model="card.expiry.month"
+                            v-autoTab
                             required
+                            :state="errors.expiryMonth.length > 0 ? false : null"
                             placeholder="00">
               </b-form-input>
             </b-form-group>
           </b-col>
           <b-col>
-          <b-form-group label="YEAR">
+          <b-form-group label="YEAR" :invalid-feedback="errors.expiryYear" :state="errors.expiryYear.length > 0 ? false : null">
             <b-form-input type="text"
+                          id="expiryYear"
                           v-numericOnly maxlength="4"
                           v-model="card.expiry.year"
+                          v-autoTab
                           required
+                          :state="errors.expiryYear.length > 0 ? false : null"
                           placeholder="0000">
             </b-form-input>
           </b-form-group>
@@ -57,11 +70,14 @@
           <b-col>
           </b-col>
           <b-col>
-            <b-form-group label="CVV">
+            <b-form-group label="CVC" :invalid-feedback="errors.cardCVC" :state="errors.cardCVC.length > 0 ? false : null">
               <b-form-input type="text"
-                            v-numericOnly maxlength="4"
+                            id="cvc"
+                            v-numericOnly maxlength="3"
                             v-model="card.cvc"
+                            v-autoTab
                             required
+                            :state="errors.cardCVC.length > 0 ? false : null"
                             placeholder="000">
               </b-form-input>
             </b-form-group>
@@ -74,8 +90,8 @@
         </div>
       </div>
       <div class="card-footer">
-        <b-row>
-          <b-col>
+        <b-row class="row-footer">
+          <b-col class="col-amount">
             <div><span class="thick">TOTAL: </span><span class="thick amount">{{ card.amount }}</span></div>
           </b-col>
           <b-col cols="4"></b-col>
@@ -112,8 +128,15 @@ let card = {
       return {
         card,
         cardTypes: ['mastercard','visa'],
-        numberSets: ["1234", "5678", "", ""],
+        numberSets: ["", "", "", ""],
+        errorInfo: "no-error",
         isError: false,
+        errors: {
+          cardNumber: "",
+          expiryMonth: "",
+          expiryYear: "",
+          cardCVC: ""
+        },
       };
     },
     methods: {
@@ -125,23 +148,27 @@ let card = {
         evt.preventDefault();
         this.card.cardNumber = this.numberSets.join('');
         this.validateForm(this.card);
+        console.log(this.errors);
       },
       validateForm(card) {
-        console.log('card info', card.amount);
-        console.log('card info', card.name.firstName);
-        console.log('card info', card.cardType);
-        console.log('card info', card.cardNumber);
-        console.log('card info', card.expiry.month);
-        console.log('card info', card.cvc);
-
         const isValidNumber = Payment.fns.validateCardNumber(card.cardNumber);
+        const isValidCardType = this.cardTypes.includes(Payment.fns.cardType(card.cardNumber));
         const isValidCVC = Payment.fns.validateCardCVC(card.cvc);
         const isValidExpiry = Payment.fns.validateCardExpiry(card.expiry.month, card.expiry.year);
 
-        console.log('card valid number', isValidNumber);
-        console.log('card valid date', isValidExpiry);
-        console.log('card valid cvc', isValidCVC);
-        
+        if(isValidCardType) {
+          this.card.cardType = Payment.fns.cardType(card.cardNumber);
+        }
+        if(!isValidNumber) {
+          this.errors.cardNumber = "Please correct your card number";
+        }
+        if(!isValidCVC) {
+          this.errors.cardCVC = "CVC number is not valid";
+        }
+        if(!isValidExpiry) {
+          this.errors.expiryMonth = "Please enter correct expiration month";
+          this.errors.expiryYear = "Please enter correct expiration year";
+        }
       }
     },
     directives: {
@@ -151,6 +178,15 @@ let card = {
             let regex = /^[0-9]*$/
             if (!regex.test(el.value)) {
               el.value = el.value.slice(0, -1)
+            }
+          })
+        }
+      },
+      autoTab: {
+        bind(el) {
+          el.addEventListener('keyup', () => {
+            if (el.value.length === el.maxLength) {
+              focusNextElement();
             }
           })
         }
@@ -169,6 +205,28 @@ let card = {
       }
     }
   };
+
+  
+  /**
+   * To enable `auto tabbing` to next element
+   * Reference : https://stackoverflow.com/a/35173443
+   */
+  function focusNextElement() {
+    //add all elements we want to include in our selection
+    var focussableElements = 'a:not([disabled]), button:not([disabled]), input[type=text]:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"])';
+    if (document.activeElement && document.activeElement.form) {
+        var focussable = Array.prototype.filter.call(document.activeElement.form.querySelectorAll(focussableElements),
+        function (element) {
+            //check for visibility while always include the current activeElement 
+            return element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement
+        });
+        var index = focussable.indexOf(document.activeElement);
+        if(index > -1) {
+            var nextElement = focussable[index + 1] || focussable[0];
+            nextElement.focus();
+        }                    
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -198,7 +256,7 @@ let card = {
       padding: 20px;
     }
     .right-container {
-      padding: 5%;
+      padding-left: 5%;
       flex: 1;
       img {
         width: 70px;
@@ -215,14 +273,24 @@ let card = {
       span.amount {
         color: #d1ac6a;
       }
+      .col-amount {
+        position: relative;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      .row-footer{
+        height: 50px;
+      }
       .pay-button {
-        width: 100%;
+        width: 70%;
         height: 100%;
         font-size: 9pt;
+        float: right;
         border: none;
         background-color: #d1ac6a;
         border-radius: 35px;
-        box-shadow: 10px 10px -10px 0px rgba(0,0,0,0.75);
+        box-shadow: 9px 9px 64px -25px rgba(0,0,0,0.75), inset 1px 1px 60px -20px rgba(253,253,253,0.75);;
+        
       }
     }
   }
